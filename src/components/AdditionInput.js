105a5580c217a3788659fb5acd832c411067a72e
MAX\marwa <marwanaboudaoud@gmail.com>
2023-09-26 10:17:34 +0200

    fix style code

--- src/components/AdditionInput.js
+++ src/components/AdditionInput.js
@@ -2,7 +2,7 @@
 export const AdditionInput = ({ label, id, value, onChange }) => {
     return (
         <div className="col-6 input-container mb-4">
-            <label className="label mb-2" id={`${id}_label`}>{label}</label>
+            <label className="mb-2" id={`${id}_label`}>{label}</label>
             <input id={id} value={value} onChange={onChange} />
         </div>
     )
